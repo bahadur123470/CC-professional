4ba930b61d046a0af7efdf04ae512e548ab54d60
Bahadur Ali <bahadurali123470@gmail.com>
2025-04-15 04:20:06 +0500

    utils code ( asyncHandler,ApiResponse,ApiError )

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,21 @@
+const asyncHandler = (requestHandler) => {
+     return (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next))
+        .catch((err) => next(err))
+    }
+}
+
+export { asyncHandler };
+
+
+// const asyncHandler = (f) => ( req , res , ext) => {
+//     try {
+
+//     }
+//     catch(error){
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message || "Internal Server Error", 
+//         })
+//     }
+// }
