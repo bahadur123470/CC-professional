3f5ea674c7c05cfc2245651878859ecfb43f2e4c
Bahadur Ali <bahadurali123470@gmail.com>
2025-04-16 19:34:22 +0500

    Setup configured

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.use(express.static("public"))
 
 app.use(cookieParser())
 
+//Routes import
+import userRouter from "./routes/user.routes.js"
+
+//Routes declaration
+app.use("/api/v1/users" , userRouter)
+
+//http://localhost:8000/api/v1/users/register
+
 export { app };
