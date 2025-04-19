8bff0c8a5a45e3c08c4bbb70ea14ea7332cc08ce
Bahadur Ali <bahadurali123470@gmail.com>
2025-04-20 04:17:31 +0500

    fs unlink

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,10 +32,14 @@ const registerUser = asyncHandler ( async (req,res) => {
             throw new ApiError(409, "User with this Username or Email already exist")
         }
         
-        
+        // console.log(req.files)
         // check for images , check for avatar
         const avatarLocalPath = req.files?.avatar[0]?.path;
         const coverImageLocalPath = req.files?.coverImage[0]?.path;
+        // let coverImageLocalPath ;
+        // if (req.files && Array.isArray (req.files.coverImage) && req.files.coverImage.length > 0 ){
+        //     coverImageLocalPath = req.files.coverImage[0].path
+        // }
         if (!avatarLocalPath) {
             throw new ApiError(400, "Avatar is required")
         }
